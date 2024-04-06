2f13148b5193a42d88aad7dbd9d70ce6ea697525
NailaImran <143532950+NailaImran@users.noreply.github.com>
2024-04-06 11:19:00 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,60 @@
+import  inquirer from "inquirer"
+
+
+let myTodo:string[] = []
+
+async function createTodo(myTodo:string[]) {
+
+do{
+    let ans = await inquirer.prompt({
+        type:"list",
+        message:"select the operation",
+        name:"select",
+        choices:["add","update","view","delete"]
+    })
+    if(ans.select == "add" ){
+        let addTodo = await inquirer.prompt({
+            type:"input",
+            message:"add item",
+            name:"todo",
+        });
+        myTodo.push(addTodo.todo)
+        console.log(myTodo)
+    }
+    if(ans.select == "update"){
+        let updateTodo = await inquirer.prompt({
+            type:"list",
+            message:"select item from update",
+            name:"todo",
+            choices:myTodo.map(item => item)
+        })
+        let addTodo = await inquirer.prompt({
+            type:"input",
+            message:"add item",
+            name:"todo",
+        });
+        let newTodos = myTodo.filter(val =>val !==updateTodo.todo)
+        myTodo = [...newTodos,addTodo.todo]
+        console.log(myTodo)
+    }
+    if(ans.select == "view"){
+        console.log(myTodo)
+    }
+    if(ans.select == "delete"){
+        let deleteTodo = await inquirer.prompt({
+            type:"list",
+            message:"select item from update",
+            name:"todo",
+            choices:myTodo.map(item => item)
+        })
+        let newTodos = myTodo.filter(val =>val !== deleteTodo.todo)
+        myTodo = [...newTodos]
+        console.log(myTodo)
+
+    
+    }
+    
+}while(true)    
+   
+}
+createTodo(myTodo)
